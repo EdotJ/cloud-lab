73606020b83209374c259c682e3fc0d53b7a9edb
Eligijus Jocius <eligijus.jocius@gmail.com>
2019-11-08 15:10:56 +0200

    fix

--- internal/controller/controller.go
+++ internal/controller/controller.go
@@ -11,7 +11,7 @@ import (
 
 func Sha256Handler(w http.ResponseWriter, r *http.Request) {
 	username := mux.Vars(r)["username"]
-	hashed, _ := hashes.GetHash("SHA256", username)
+	hashed, _ := hashes.GetHash("Sha256", username)
 	//username := mux.Vars(r)["username"]
 
 	// to calculate sha256 hash of a string, use internal/hashes package and function GetHash
@@ -19,6 +19,7 @@ func Sha256Handler(w http.ResponseWriter, r *http.Request) {
 	// to send the response, use the following:
 	// fmt.Fprint(w, "your response here")
 	w.WriteHeader(http.StatusOK)
+
 	fmt.Fprint(w, hashed)
 }
 
